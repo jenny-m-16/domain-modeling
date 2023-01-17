d93d59e3db0cf3f40341986fec986b505823de02
jenny-m-16 <jenny.mak@kellogg.northwestern.edu>
2023-01-17 22:14:27 +0000

    practiced some in class

--- crm/crm.sql
+++ crm/crm.sql
@@ -2,4 +2,52 @@
 -- run of this script
 -- e.g. DROP TABLE IF EXISTS ______;
 
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS activity;
+DROP TABLE IF EXISTS company;
+DROP TABLE IF EXISTS salespersons;
+DROP TABLE IF EXISTS industry;
+DROP TABLE IF EXISTS industries_companies;
+
 -- CREATE TABLES
+CREATE TABLE contacts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    title TEXT,
+    email TEXT,
+    phone TEXT,
+    company_id INTEGER
+)
+
+CREATE TABLE activity (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    type TEXT,
+    occured_at TEXT,
+    notes TEXT,
+    salespersons_id INTEGER,
+    contact_id INTEGER
+)
+
+CREATE TABLE company (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    company TEXT
+)
+
+CREATE TABLE salespersons (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT
+)
+
+CREATE TABLE industry (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    industry TEXT
+)
+
+CREATE TABLE industries_companies ( 
+    id INTEGER PRIMARY KEY AUTOINCREMENT
+    industry_id TEXT,
+    company_id TEXT
+) 
